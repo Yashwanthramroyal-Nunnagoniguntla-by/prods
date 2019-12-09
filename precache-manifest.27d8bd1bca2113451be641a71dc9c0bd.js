0a0cbe66a2ff569f9cb378dc826d2ecead410c5c
Yashwanthramroyal Nunnagoniguntla <yn19114@corp.ojas-it.com>
2019-12-09 19:08:48 +0530

    Updates

---
+++ precache-manifest.27d8bd1bca2113451be641a71dc9c0bd.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc3270277acc49d43fbb510d7921a3cb",
+    "url": "/yashwanthramroyal/prods.git/index.html"
+  },
+  {
+    "revision": "8b6ccf9b1bcb69acc2b6",
+    "url": "/yashwanthramroyal/prods.git/static/css/2.e082a319.chunk.css"
+  },
+  {
+    "revision": "a04202cb8b8d3b68b8a4",
+    "url": "/yashwanthramroyal/prods.git/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8b6ccf9b1bcb69acc2b6",
+    "url": "/yashwanthramroyal/prods.git/static/js/2.5bd6c577.chunk.js"
+  },
+  {
+    "revision": "a04202cb8b8d3b68b8a4",
+    "url": "/yashwanthramroyal/prods.git/static/js/main.a4986173.chunk.js"
+  },
+  {
+    "revision": "0474ad6b965e14b6b0e7",
+    "url": "/yashwanthramroyal/prods.git/static/js/runtime-main.853ae64a.js"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/yashwanthramroyal/prods.git/static/media/fa-solid-900.c2801fb4.svg"
+  }
+]);
